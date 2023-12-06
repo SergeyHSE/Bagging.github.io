a24af140f72fd3350ec4949e361258ab8efd8b09
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-12-06 11:04:20 +0300

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -102,13 +102,6 @@ plt.show()
 In this part we are gonna work with anothe dataset 'data.csv'
 """
 
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import StandardScaler
-import seaborn as sns
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 
 """
